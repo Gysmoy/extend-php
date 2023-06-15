dc2c0b34e2a14ab4a6da9b783ea9ce9ab92c28e2
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-06-15 09:54:03 -0500

    Actualizando la clase Crypto

--- src/Crypto.php
+++ src/Crypto.php
@@ -20,17 +20,53 @@ class Crypto
     }
 
     /**
-     * Función que genera un id largo
-     * @return string - una cadena de 36 caracteres.
+     * Genera un UUID (Universally Unique Identifier) aleatorio en su versión 4.
+     *
+     * @return string Un UUID aleatorio en su versión 4.
      */
-    static public function ramdomUUID(): string
+    public static function randomUUID(): string
     {
-        $c1 = bin2hex(openssl_random_pseudo_bytes(4));
-        $c2 = bin2hex(openssl_random_pseudo_bytes(2));
-        $c3 = bin2hex(openssl_random_pseudo_bytes(2));
-        $c4 = bin2hex(openssl_random_pseudo_bytes(2));
-        $c5 = bin2hex(openssl_random_pseudo_bytes(6));
+        $bytes = openssl_random_pseudo_bytes(16);
+        $bytes[6] = chr(ord($bytes[6]) & 0x0F | 0x40); // Set the version to 4
+        $bytes[8] = chr(ord($bytes[8]) & 0x3F | 0x80); // Set the variant
 
-        return "{$c1}-{$c2}-{$c3}-{$c4}-{$c5}";
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
+    }
+
+
+    /**
+     * Genera un token de acceso seguro.
+     *
+     * @param int $length La longitud del token (predeterminado: 32).
+     * @return string Un token de acceso seguro.
+     */
+    public static function generateAccessToken(int $length = 32): string
+    {
+        $bytes = openssl_random_pseudo_bytes($length);
+        return bin2hex($bytes);
+    }
+
+    /**
+     * Genera una clave de cifrado segura.
+     *
+     * @param int $length La longitud de la clave (predeterminado: 16).
+     * @return string Una clave de cifrado segura.
+     */
+    public static function generateEncryptionKey(int $length = 16): string
+    {
+        $bytes = openssl_random_pseudo_bytes($length);
+        return base64_encode($bytes);
+    }
+
+    /**
+     * Genera un token CSRF (Cross-Site Request Forgery) seguro.
+     *
+     * @param int $length La longitud del token (predeterminado: 16).
+     * @return string Un token CSRF seguro.
+     */
+    public static function generateCSRFToken(int $length = 16): string
+    {
+        $bytes = openssl_random_pseudo_bytes($length);
+        return bin2hex($bytes);
     }
 }
