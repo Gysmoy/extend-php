875c43621a1555208ed32732b4df6ad09c6f8c71
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-08-13 12:30:38 -0500

    corrección de la clase JWT

--- src/JWTSoDe.php
+++ src/JWTSoDe.php
@@ -4,17 +4,19 @@ namespace SoDe\Extend;
 
 class JWTSoDe
 {
-    static public function getToken(): string
-    {
+    static private function password():string {
         $sign = 'f2c408d6-6d07-47e0-ab4c-7c082a54722c';
         $date = Trace::getDate('date');
-        return password_hash($sign . $date, PASSWORD_DEFAULT);
+        return $sign . $date;
+    }
+    static public function hash(): string
+    {
+        return password_hash(JWTSoDe::password(), PASSWORD_DEFAULT);
     }
 
     static public function verify(string $token): bool
     {
-        $hash = password_hash(JWTSoDe::getToken(), PASSWORD_DEFAULT);
-        $token = str_replace('Bearer ', '', $token);
-        return password_verify($token, $hash);
+        $hash = str_replace('Bearer ', '', $token);
+        return password_verify(JWTSoDe::password(), $hash);
     }
 }
