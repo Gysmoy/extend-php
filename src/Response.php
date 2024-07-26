2b75160c0b8603351e5ba85375bf9f071a3e0149
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-07-26 11:32:14 -0500

    Agregando un metodo simpletrycatch

--- src/Response.php
+++ src/Response.php
@@ -2,7 +2,8 @@
 
 namespace SoDe\Extend;
 
-class Response {
+class Response
+{
     public int $status;
     public string $message;
     public int $draw;
@@ -10,17 +11,41 @@ class Response {
     public int $iTotalRecords;
     public object|array|string|int|float|bool|null $data;
 
-    public function __construct() {
-        $this -> status = 500;
-        $this -> message = 'Error inesperado';
-    }
-    public function toArray(): array {
-        $json = json_encode($this);
-        $array = json_decode($json, true);
-        return $array;
+    public function __construct()
+    {
+        $this->status = 500;
+        $this->message = 'Error inesperado';
     }
 
-    function __toString()
+    public function toArray(): array
+    {
+        $json = json_encode($this);
+        return json_decode($json, true);
+    }
+
+    public static function simpleTryCatch(callable $callback): self
+    {
+        $response = new self();
+        try {
+            $data = $callback($response);
+            if (isset($data)) {
+                $response->data = $data;
+            }
+            if ($response->status === 500) {
+                $response->status = 200;
+            }
+            if ($response->message === 'Error inesperado') {
+                $response->message = 'Operación correcta';
+            }
+        } catch (\Throwable $th) {
+            $response->status = 400;
+            $response->message = $th->getMessage();
+        } finally {
+            return $response;
+        }
+    }
+
+    public function __toString(): string
     {
         return json_encode($this);
     }
